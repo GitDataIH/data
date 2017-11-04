310f38707379fa0e79d567b534c3c978cceb129f
GitDataIH <GitDataIH@users.noreply.github.com>
2017-11-04 01:11:01 -0400

    Add files via upload

---
+++ warning.js
@@ -0,0 +1,1 @@
+if(window.console&&window.console.log){window.console.log("%cArrêtez! Cette zone est destinée aux développeurs!","color: red; font-size: xx-large");window.console.log("Si quelqu'un vous a dit de taper ou de coller quelque chose dans cette zone, cela pourrait compromettre la sécurité de votre compte.");}
